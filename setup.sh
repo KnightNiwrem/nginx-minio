f5c245e0c0fae7fd4db9d38be24b7ab9d9df03f0
KnightNiwrem <merwinwee@gmail.com>
2019-09-07 17:38:18 +0800

    Don't try to make symbolic link, fix docker-compose

--- setup.sh
+++ setup.sh
@@ -13,4 +13,3 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 # Install Docker-Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
